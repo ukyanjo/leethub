beb772940196d7094da4c2cabcf5e3df37636c74
조욱현 <105177597+ukyanjo@users.noreply.github.com>
2024-07-21 20:08:49 +0900

    Time: 3918 ms (5.04%), Space: 22.3 MB (8.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,23 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = collections.defaultdict(list)
+        sortedStrs = []
+        for str in strs:
+            sortedStrs.append(''.join(sorted(str)))     
+        trimmedStrs = list(set(sortedStrs))
         
-        for word in strs:
-            anagrams[''.join(sorted(word))].append(word)
-            
-        return list(anagrams.values())
+        tempReturnList = [[str] for str in trimmedStrs]
+        tempIdxList = [[] for idx in range(len(trimmedStrs))]
+        returnList = [[] for idx in range(len(trimmedStrs))]
+        
+        tupledStrs = [(idx, str) for idx, str in enumerate(sortedStrs)]
+        for tupledStr in tupledStrs:
+            idxT, sortedStr = tupledStr
+            for idxL,tempList in enumerate(tempReturnList):
+                if sortedStr == tempList[0]:
+                    tempIdxList[idxL].append(idxT)
+                    
+        for idxL, idxList in enumerate(tempIdxList):
+            for idx in idxList:
+                returnList[idxL].append(strs[idx])
+                
+        return returnList
