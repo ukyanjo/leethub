653e1a2a85f97b63f982f3021eeb3f90e56570ae
조욱현 <105177597+ukyanjo@users.noreply.github.com>
2024-07-22 23:09:10 +0900

    Time: 9541 ms (5.00%), Space: 214.9 MB (6.06%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        tempDict = defaultdict(list)
+        for num in range(1, len(s)+1):
+            for idx in range(len(s)-(num-1)):
+                if(s[idx:idx+num] == s[idx:idx+num][::-1]):
+                    tempDict[num].append(s[idx:idx+num])    
+                    
+        return random.choice(tempDict[list(tempDict.keys())[-1]])
